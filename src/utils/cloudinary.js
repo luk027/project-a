3b15af50a6cfad0c636bdfae1c182e3c0457ac85
Luk027 <lookman.gohil@gmail.com>
2024-05-28 18:48:49 +0530

    multer and cloudinary added for file uploading

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,62 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs"
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        //upload file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type:'auto'
+        })
+        //file has been uploaded successfull
+        console.log("file Uploaded on cloudinary ", response.url);
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove locally saved temp file as the upload operation got failed.
+        return null;
+    }
+}
+
+export {cloudinary}
+
+// (async function() {
+
+//     // Configuration
+//     cloudinary.config({ 
+//         cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+//         api_key: process.env.CLOUDINARY_API_KEY, 
+//         api_secret: process.env.CLOUDINARY_API_SECRET
+//     });
+    
+//     // Upload an image
+//     const uploadResult = await cloudinary.uploader.upload("https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg", {
+//         public_id: "shoes"
+//     }).catch((error)=>{console.log(error)});
+    
+//     console.log(uploadResult);
+    
+//     // Optimize delivery by resizing and applying auto-format and auto-quality
+//     const optimizeUrl = cloudinary.url("shoes", {
+//         fetch_format: 'auto',
+//         quality: 'auto'
+//     });
+    
+//     console.log(optimizeUrl);
+    
+//     // Transform the image: auto-crop to square aspect_ratio
+//     const autoCropUrl = cloudinary.url("shoes", {
+//         crop: 'auto',
+//         gravity: 'auto',
+//         width: 500,
+//         height: 500,
+//     });
+    
+//     console.log(autoCropUrl);    
+// })();
